5f89149c436f24bb20547689ee158e59bad74f5b
csubocz78-arch <csubocz78@gmail.com>
2026-07-17 04:31:40 -0400

    Add new folder

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -3,7 +3,7 @@ import json
 
 def emotion_detector(text_to_analyze):
     URL = "https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict"
-    header = {"grpc-metadata-mm-model-id": "emotion_aggregate-workflow_lang_en_stock"}
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input_json = {"raw_document": {"text": text_to_analyze}}
 
     response = requests.post(URL, json=input_json, headers = header)
